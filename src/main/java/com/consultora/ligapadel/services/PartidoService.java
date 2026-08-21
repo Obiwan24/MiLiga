42d79e7b7164b3a72cc0256f0bb09da481531bd0
Obiwan24 <el.tito.ivi90@gmail.com>
2026-08-22 00:31:34 +0200

    actualizacion 220031AGO26

--- src/main/java/com/consultora/ligapadel/services/PartidoService.java
+++ src/main/java/com/consultora/ligapadel/services/PartidoService.java
@@ -1,17 +1,22 @@
 package com.consultora.ligapadel.services;
 
+import com.consultora.ligapadel.repositories.EnfrentamientoRepository;
 import com.consultora.ligapadel.repositories.EquipoRepository;
+import com.consultora.ligapadel.repositories.InscripcionRepository;
 import com.consultora.ligapadel.repositories.PartidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PartidoService {
-    @Autowired
-    private EquipoRepository equipoRepository;
 
     @Autowired
     private PartidoRepository partidoRepository;
 
+    @Autowired
+    private EnfrentamientoRepository enfrentamientoRepository;
+
+    @Autowired
+    private InscripcionRepository inscripcionRepository;
 
 }
